34bd7bca465d2b5bc059f756c12fbf666fb338ad
Nanang Arifudin <nanangarif404@gmail.com>
2022-04-21 01:24:55 +0700

    layout detail movie

--- app/src/main/java/com/android/movku/presentation/movie/detail/DetailMovieActivity.kt
+++ app/src/main/java/com/android/movku/presentation/movie/detail/DetailMovieActivity.kt
@@ -2,11 +2,17 @@ package com.android.movku.presentation.movie.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.android.movku.R
+import com.android.movku.databinding.ActivityDetailMovieBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailMovieActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityDetailMovieBinding.inflate(layoutInflater)}
+    val viewModel by viewModels<DetailMovieViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_movie)
+        setContentView(binding.root)
     }
 }
